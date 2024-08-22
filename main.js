efce6bc74ba869aa4eb00d27070c92b1b8c644a1
Giresse FOYET <ffgire@gmail.com>
2024-08-23 00:49:47 +0100

    nouveau fichier

---
+++ main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className == "navbar") {
+        x.className += " respons";
+    } else {
+        x.className = "navbar";
+    }
+} 
